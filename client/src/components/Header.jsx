dd10eb8bb4b368fcdae2d16902faf293c92af834
JAGADEESH <22022cm040@gmail.com>
2025-06-16 10:32:36 +0530

    PJ |  add the header component structure html

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="headerDiv">
+            <h1>Meghana Silk Sarees</h1>
+            <button>SignUp</button>
+            <button>Login</button>
+            <button>🛒</button>
+        </div>
+    );
+}
+
+export default Header;
