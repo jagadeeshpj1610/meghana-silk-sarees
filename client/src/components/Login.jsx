06de1effcc5003c61ab1b5e421fc8fd1b6960d41
JAGADEESH <22022cm040@gmail.com>
2025-06-16 14:49:11 +0530

    PJ |  add the css  for signup and modify some changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,31 @@
 import '../css/login.css';
 import { Link } from 'react-router-dom';
+import Header from './Header';
 
 const Login = () => {
     return (
-        <div className="loginDiv">
-            <h2>Login</h2>
-            <form>
-                <div className="formGroup">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" className="email" required name="email_no_autofill" autoComplete="off" />
+        <>
+            <Header />
+            <div className="loginDiv">
+                <h2>Login</h2>
+                <form>
+                    <div className="formGroup">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" className="email" required autoComplete="off" />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" className="password" required autoComplete="new-password" />
+                    </div>
+                    <div className="loginButtonDiv">
+                        <button type="submit">Login</button>
+                    </div>
+                    <div className="signupRedirect">
+                        <p>Don't have an account? <Link to="/signup">Sign Up</Link></p>
+                    </div>
+                </form>
+            </div>
+        </>
 
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" className="password" name="password_no_autofill" autoComplete="new-password" required />
-                </div>
-                <div className='loginButtonDiv'>
-                    <button type="submit">Login</button>
-                    <Link className='signupLink' to='/signup'><p className='signupPrompt'>if you don't have an account Sign Up</p></Link>
-                </div>
-            </form>                     
-        </div>
     );
-
 };
 
 export default Login;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,28 +1,29 @@
-
+import { Link } from "react-router-dom";
+import '../css/signup.css';
+import Header from "./Header";
 
 const SignUp = () => {
     return (
-        <div className="signUpDiv">
-            <h2>Sign Up</h2>
-            <form>
-                <div className="formGroup">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" className="name" required name="name_no_autofill" autoComplete="off" />
-                </div>
-                <div className="formGroup">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" className="email" required name="email_no_autofill" autoComplete="off" />
-                </div>
-                <div className="formGroup">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" className="password" name="password_no_autofill" autoComplete="new-password" required />
-                </div>
-                <button type="submit">Sign Up</button>
-            </form>                 
-        </div>
+        <>
+        <Header />
+            <div className="signUpDiv">
+                <h2>Sign Up</h2>
+                <form>
+                    <div className="formDiv">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" className="name" required autoComplete="off" />
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" className="email" required autoComplete="off" />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" className="password" required autoComplete="new-password" />
+                        <button type="submit">Sign Up</button>
+                        <p className="loginRedirect">Already have an account? <Link to="/login">Login</Link></p>
+                    </div>
+                </form>
+            </div>
+        </>
+
     );
-
 }
-
 
 export default SignUp;
