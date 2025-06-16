c6d84f9bc01d0b8025dbdfba037186fdc480f3e9
JAGADEESH <22022cm040@gmail.com>
2025-06-16 12:59:48 +0530

    PJ |  add the css  for login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,5 @@
-
-
+import '../css/login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
@@ -9,16 +9,18 @@ const Login = () => {
                 <div className="formGroup">
                     <label htmlFor="email">Email:</label>
                     <input type="email" className="email" required name="email_no_autofill" autoComplete="off" />
-                </div>
-                <div className="formGroup">
+
                     <label htmlFor="password">Password:</label>
                     <input type="password" className="password" name="password_no_autofill" autoComplete="new-password" required />
                 </div>
-                <button type="submit">Login</button>
-            </form>
+                <div className='loginButtonDiv'>
+                    <button type="submit">Login</button>
+                    <Link className='signupLink' to='/signup'><p className='signupPrompt'>if you don't have an account Sign Up</p></Link>
+                </div>
+            </form>                     
         </div>
     );
 
-}
+};
 
 export default Login;
