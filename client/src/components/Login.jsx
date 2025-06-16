2d214cb70b24886ae7a72d8367ee3074de257b3e
JAGADEESH <22022cm040@gmail.com>
2025-06-16 11:06:35 +0530

    PJ |  add the html structure for login and signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,12 +3,22 @@
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className="loginDiv">
+            <h2>Login</h2>
+            <form>
+                <div className="formGroup">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" className="email" required name="email_no_autofill" autoComplete="off" />
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" className="password" name="password_no_autofill" autoComplete="new-password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 
 }
-
 
 export default Login;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -2,8 +2,23 @@
 
 const SignUp = () => {
     return (
-        <div>
-            SignUP
+        <div className="signUpDiv">
+            <h2>Sign Up</h2>
+            <form>
+                <div className="formGroup">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" className="name" required name="name_no_autofill" autoComplete="off" />
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" className="email" required name="email_no_autofill" autoComplete="off" />
+                </div>
+                <div className="formGroup">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" className="password" name="password_no_autofill" autoComplete="new-password" required />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>                 
         </div>
     );
 
