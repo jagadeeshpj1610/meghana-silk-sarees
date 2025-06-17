a96e615d8fad2764ab17ec3644ed1f9524c7ce36
JAGADEESH <22022cm040@gmail.com>
2025-06-17 12:39:04 +0530

    PJ |  due to unknown route hanlers commented it

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -2,12 +2,11 @@ import express from "express";
 import { login, signup } from "../controllers/authController.js";
 const authRouter = express.Router();
 
-<<<<<<< HEAD
+
 authRouter.post('/login', login)
 authRouter.post('/signup', signup)
-=======
 // authRouter.post('/login', )
 // authRouter.post('/signup', )
->>>>>>> 6ada71a (PJ |  test the upload saree image)
+
 
 export default authRouter;
