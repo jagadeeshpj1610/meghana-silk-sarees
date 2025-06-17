b62a3473643eaec63a5da9aff82062b220c0d672
JAGADEESH <22022cm040@gmail.com>
2025-06-17 15:08:44 +0530

    PJ |  comment the code admin feature upload

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className='homePage'>
             <Header />
-            <UploadSaree />
+            {/* <UploadSaree /> */}
             <Sarees />
          
         </div>
--- client/src/components/upload.jsx
+++ client/src/components/upload.jsx
@@ -1,48 +1,48 @@
 
 
-import { useState } from "react";
+// import { useState } from "react";
 
 
 
 
-    const UploadSaree = () =>  {
-        const [file, setFile] = useState(null);
+//     const UploadSaree = () =>  {
+//         const [file, setFile] = useState(null);
 
-        const handleFileChange = (e) => {
-            setFile(e.target.files[0]);
-        };
+//         const handleFileChange = (e) => {
+//             setFile(e.target.files[0]);
+//         };
 
-        const handleUpload = async () => {
-            if (!file) console.log("file required");
+//         const handleUpload = async () => {
+//             if (!file) console.log("file required");
             
 
-            const formData = new FormData();
-            formData.append('saree-image', file);
+//             const formData = new FormData();
+//             formData.append('saree-image', file);
 
-            try {
-                const res = await fetch('http://localhost:8000/upload', {
-                    method: 'POST',
-                    body: formData,
-                });
+//             try {
+//                 const res = await fetch('http://localhost:8000/upload', {
+//                     method: 'POST',
+//                     body: formData,
+//                 });
 
-                const data = await res.json();
-                console.log("Upload Success:", data);
-                console.log("uploaded succesfully");
+//                 const data = await res.json();
+//                 console.log("Upload Success:", data);
+//                 console.log("uploaded succesfully");
                 
-            } catch (error) {
-                console.log("Upload failed:", error);
+//             } catch (error) {
+//                 console.log("Upload failed:", error);
                 
-            }
-        };
+//             }
+//         };
 
-        return (
-            <div>
-                <h2>Upload Silk Saree</h2>
-                <input type="file" accept="image/*" onChange={handleFileChange} />
-                <button onClick={handleUpload}>Upload</button>
-            </div>
-        );
-    }
+//         return (
+//             <div>
+//                 <h2>Upload Silk Saree</h2>
+//                 <input type="file" accept="image/*" onChange={handleFileChange} />
+//                 <button onClick={handleUpload}>Upload</button>
+//             </div>
+//         );
+//     }
 
 
-export default UploadSaree
+// export default UploadSaree
