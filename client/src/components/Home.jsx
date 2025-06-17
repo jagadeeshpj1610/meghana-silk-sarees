e5ffc1c908d85ba03086a5fdc2567c7e82070923
JAGADEESH <22022cm040@gmail.com>
2025-06-17 12:36:25 +0530

    PJ |  test the upload saree image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
-import Sarees from './Sarees';
+import UploadSaree from './Sarees'
 
 const Home = () => {
     return (
         <div className='homePage'>
             <Header />
-            <Sarees />
+            <UploadSaree />
+
+         
         </div>
     );
 }
