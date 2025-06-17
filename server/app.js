22e8e973ed75f8bd755be206a32a850673384dee
Hemeswar reddy <172027872+hemeswar9849@users.noreply.github.com>
2025-06-17 11:18:08 +0530

    Hems | put cors policy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import connectToDB from "./src/config/db.js";
 import { uploadRouter } from "./src/routers/uploadRouter.js";
+import cors from "cors";
 const app = express();
 
 connectToDB();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true
+}))
 
 app.use(express.json());
 app.use('/upload',uploadRouter);
