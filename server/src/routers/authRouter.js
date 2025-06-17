0d16b87b6bfad09cf6ecfa5e7e33cbba487dd9f5
Hemeswar reddy <172027872+hemeswar9849@users.noreply.github.com>
2025-06-17 15:34:28 +0530

    Hems | created router for loggedIn

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { isLoggedIn, login, logout, signup } from "../controllers/authController.js";
 const authRouter = express.Router();
 
 
-authRouter.post('/login', login)
-authRouter.post('/signup', signup)
+authRouter.post('/login', login);
+authRouter.post('/signup', signup);
 authRouter.post('/logout', logout);
+authRouter.get('/isLoggedIn', isLoggedIn);
 
 
 export default authRouter;
