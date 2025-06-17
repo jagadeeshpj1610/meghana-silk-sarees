b40fa3e271c998cbac93aedabc717ede31c0d595
JAGADEESH <22022cm040@gmail.com>
2025-06-17 23:04:47 +0530

    PJ |  add the sareee card css

--- client/src/components/SareeDetails.jsx
+++ client/src/components/SareeDetails.jsx
@@ -1,12 +1,14 @@
+import '../css/sarees.css'
 
-
-const SareeDetails = ({sareesInfo}) => {    
-    return(
+const SareeDetails = ({ sareesInfo }) => {
+    return (
         <div className="sareeDetails">
-            <p>Title : {sareesInfo.sareeName}</p>
+            <p style={{ textAlign: "left" }}>Title : {sareesInfo.sareeName}</p>
             <p>Price : {sareesInfo.sareePrice}</p>
-            <button>Add to Cart</button>
-            <button>Buy</button>
+            <div className='buyBtns'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </div>
         </div>
     )
 }
