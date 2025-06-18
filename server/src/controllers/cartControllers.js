9c9cf2bc00f0c1a675f357bcc9fc976adc8831f1
Hemeswar reddy <172027872+hemeswar9849@users.noreply.github.com>
2025-06-18 15:47:52 +0530

    Hems | created basic routes and controllers

--- server/src/controllers/cartControllers.js
+++ server/src/controllers/cartControllers.js
@@ -1,7 +1,20 @@
 import cartModel from "../models/cartModel";
 
+const addToCart = (req, res) => {
+
+}
+
+const getAllCarts = (req, res) => {
+
+}
+
+const removeCart = (req, res) => {
+
+}
 
 
 export {
-
+  addToCart,
+  removeCart,
+  getAllCarts,
 }
