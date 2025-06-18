69298de0fc921e7aebe683dd974c10a7b8b050a6
Hemeswar reddy <172027872+hemeswar9849@users.noreply.github.com>
2025-06-18 15:57:59 +0530

    Hems | little change in cart model

--- server/src/models/cartModel.js
+++ server/src/models/cartModel.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const cartSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   card: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Card",
