9df16c43303b7b5b6697bd90f3fd4b26c9b93552
JAGADEESH <22022cm040@gmail.com>
2025-06-27 10:30:11 +0530

    PJ |  Add the transaction history in profile page

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -2,27 +2,30 @@ import { useState } from 'react';
 import '../css/header.css'
 import Transactions from './transactions';
 import { useUser } from './userContext';
+import '../css/profile.css'
 
 const Profile = () => {
-    const {userDetails} = useUser()
+    const { userDetails } = useUser()
 
     // console.log(userDetails);
-    
 
-    if (!userDetails) return <div style={{textAlign:'center', padding:'15px', fontSize:'1rem'}}>Loading...</div>;
+
+    if (!userDetails) return <div style={{ textAlign: 'center', padding: '15px', fontSize: '1rem' }}>Loading...</div>;
 
     return (
-        <div>
-            <div>
+        <div className='wholeContainer'>
+            <div className='transactions'>
                 <Transactions />
             </div>
-            <div className="profilePage">
+            <div className="profileDiv">
+                <h2>User Profile</h2>
                 <p>Name: {userDetails.name}</p>
                 <p>Email: {userDetails.email}</p>
                 <p>Phone: {userDetails.phone}</p>
             </div>
         </div>
-        
+
+
     );
 }
 
--- client/src/components/transactions.jsx
+++ client/src/components/transactions.jsx
@@ -1,10 +1,50 @@
+import { useEffect, useLayoutEffect } from "react"
+import { useState } from "react"
+import '../css/profile.css'
 
 
 
 const Transactions = () => {
-    return(
+    const [transactions, setTransactions] = useState([])
+    const fetchUserTransactions = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/payment', {
+                method: 'GET',
+                credentials: 'include'
+            })
+            const data = await response.json()
+            console.log(data);
+            setTransactions(data.orders)
+
+        } catch (error) {
+            console.log("Failed fectching transactions", error);
+
+        }
+    }
+    useEffect(() => {
+        fetchUserTransactions()
+    }, [])
+
+    return (
         <div>
-            <h1>Transaction</h1>
+            <h2 style={{ marginBottom: '10px' }}>My Transactions</h2>
+            {transactions && transactions.length > 0 ? (
+                <div className='cardsContaine'>
+                    {transactions.map((transaction, index) => (
+                        <div key={index} className='productContainer'>
+                            <img src={transaction.card.sareePhoto.url} alt="" className='sareeImage' />
+                            <div className='productDetails'>
+                                <p className='sareeName'><strong>Name:</strong> {transaction.card.sareeName}</p>
+                                <p className='sareePrice'><strong>Price:</strong> ₹{transaction.card.sareePrice}</p>
+                                <p className='sareePrice'><strong>Order Id:</strong> {transaction.orderId}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            ) : (
+                <p style={{ textAlign: 'center', padding: '10px', fontSize: '1rem' }}>Your cart is empty.</p>
+            )}
         </div>
     )
 }
