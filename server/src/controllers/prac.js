9a8bf05ed35dfd4b6e94d5157cb73aa8d1e3e8f0
Daemon Targaryen <157202138+Daemon-Targaryen-Caraxes@users.noreply.github.com>
2025-07-02 11:00:54 +0530

    Hems | deleted a unecessary file

--- server/src/controllers/prac.js
+++
@@ -1,8 +0,0 @@
-const arr = ["hemeswar", "meghana", "rajeswari", "madhava"];
-
-const matched = arr.filter((element) => {
-  console.log(element)
-  return element.includes("");
-})
-
-console.log(matched)
